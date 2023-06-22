2f66f5e8fab490d6d594ef0e8eb1a380df9ef6aa
Jhonnatan <analistadesarrollo5@agaval.com.co>
2023-06-21 23:22:55 -0500

    Primer Comentario

---
+++ CleanArchitecture.Data/StreamerDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Data
+{
+    public class StreamerDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(@"Server=DES-AUX-01\\\\SQLSERVIDOR;Initial Catalog=Streamer;Persist Security Info=True;User Id=sa;Password=dimitriv3g4s\");
+            optionsBuilder.UseSqlServer(@"Server=DES-AUX-01;Database=Streamer;Integrated Security=True;TrustServerCertificate=True;User Id=sa;Password=dimitriv3g4s");
+        }
+        public DbSet<Streamer>? Streamers { get; set; }
+        public DbSet<Video>? Videos { get; set; }
+
+    }
+}
